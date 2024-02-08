a1d5df9861a4b4c1719509cccdfaa0f5ecb9c860
Dawit Nigussie <dawit9n@gmail.com>
2024-02-08 06:00:21 -0800

    Add: 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints string
+ *
+ * Return: 0 exits the program
+ */
+int main(void)
+{
+	char str[200];
+
+	strcpy(str, "Programming is like building a multilingual puzzle");
+	puts(str);
+	return (0);
+}
