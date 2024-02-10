3d7075adbfd5a3a91dc48622abfd5f35b933f48c
Dawit Nigussie <dawit9n@gmail.com>
2024-02-09 22:30:55 -0800

    Add: 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+
+	while (j < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (j != i && j < i)
+			{
+				putchar(j);
+				putchar(i);
+				if (i == 57 && j == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
