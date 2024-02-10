754e214a8e8697919ac8a388ae03ac3aaf967528
Dawit Nigussie <dawit9n@gmail.com>
2024-02-09 23:50:11 -0800

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lower case
+ *
+ * Return: Void
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
