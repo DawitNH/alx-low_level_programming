bb4322f1509fa06dc7f58c4b86f5ef361618879c
Dawit Nigussie <dawit9n@gmail.com>
2024-02-09 23:40:18 -0800

    Add: 0-putchar.c, main.h and _putchar.c files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description:
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+	{
+		_putchar(word[ch]);
+	}
+	_putchar('\n');
+	return (0);
+}
