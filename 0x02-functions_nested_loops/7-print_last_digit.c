c95f6c19f10c4ce5f317f65982138835a1a0bd6a
Dawit Nigussie <dawit9n@gmail.com>
2024-02-10 22:05:11 -0800

    Fix error, in case of INT_MIN argument

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int last_dig, abs;
+	int last_dig;
 
+	last_dig = n % 10;
 	if (n < 0)
 	{
-		abs = n * -1;
-		last_dig = abs % 10;
-	} else
-	{
-		last_dig = n % 10;
+		last_dig = last_dig * -1;
 	}
 	_putchar(last_dig + '0');
 	return (last_dig);
