c8f1c6eca1a99557859cd6364a2b10378da8f356
Dawit Nigussie <dawit9n@gmail.com>
2024-02-10 21:18:16 -0800

    Add: 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - Computes absolute value of a number
+ *
+ * @n: the number to be checked
+ * Description:
+ * Return: Always 0(Success)
+ */
+int _abs(int n)
+{
+	int result;
+
+	if (n < 0)
+	{
+		result = n * -1;
+		return (result);
+	} else if (n >= 0)
+	{
+		return (n);
+	}
+}
