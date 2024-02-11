a6f371c8aa0fd1c031c087c03ba50bc96c438f82
Dawit Nigussie <dawit9n@gmail.com>
2024-02-10 23:36:10 -0800

    Fixed the comma at the end

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,6 +14,11 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
+			if (i == 98)
+			{
+				printf("%d", i);
+				break;
+			}
 			printf("%d, ", i);
 		}
 	} else if (n == 98)
@@ -23,6 +28,11 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
+			if (i == 98)
+			{
+				printf("%d", i);
+				break;
+			}
 			printf("%d, ", i);
 		}
 	}
