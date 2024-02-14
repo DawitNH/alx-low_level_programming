8aa934d9bb4c60c65e450e8bc550295493b01451
Dawit Nigussie <dawit9n@gmail.com>
2024-02-14 12:49:47 +0300

    Add: 10-print_triangle.c, 100-prime_factor.c 101-print_number.c file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints largest prime factor
+ * Return: Always 0.
+ **/
+int main(void)
+{
+	long int n, fact;
+
+	n = 612852475143;
+	for (fact = 2; fact <= n; fact++)
+	{
+		if (n % fact == 0)
+		{
+			n /= fact;
+			fact--;
+		}
+	}
+	printf("%ld", fact);
+	printf("\n");
+	return (0);
+}
