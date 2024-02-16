3cb74c9b3e88a3d2816fd14c14efda33bcd6a4d7
Dawit Nigussie <dawit9n@gmail.com>
2024-02-15 23:09:34 -0800

    Add: 0-strcat.c file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ *
+ * @dest: destination
+ * @src: Source
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int destCount, srcCount;
+
+	destCount = 0;
+	srcCount = 0;
+	while(*(dest + destCount) != '\0')
+	{
+		destCount++;
+	}
+	while(destCount >= 0)
+	{
+		*(dest + destCount) = *(src + srcCount);
+		if (*(src + srcCount) == '\0')
+			break;
+		destCount++;
+		srcCount++;
+	}
+	return (dest);
+}
