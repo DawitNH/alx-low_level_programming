6c624941ce4884a5a28b4509cbcfcb7e15fbbc9f
Dawit Nigussie <dawit9n@gmail.com>
2024-02-16 12:02:15 +0300

    Adding c files

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets a value of a pointer to a char
+ * @s: source address
+ * @to: destination address
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
